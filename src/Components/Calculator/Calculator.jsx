a174bbfc3ff2898d0f52e92111e003e5c72e8f3a
barez azad <barez.001159544@gmail.com>
2025-04-05 20:36:42 +0300

    Complete ui created

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,8 +1,35 @@
 import "./Calculator.css";
+import ButtonBox from "./ButtonBox";
+import Button from "./Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 const Calculator = () => {
   return (
     <div className="bg">
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <div className="screen"></div>
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })}
+        </ButtonBox>
+      </div>
     </div>
   );
 };
